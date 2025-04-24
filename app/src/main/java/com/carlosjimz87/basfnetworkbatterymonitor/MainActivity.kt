4910bc8d5f5e221df5752b3413dc27b76c864558
Carlos Jiménez <carlos.jimenezp@vasscompany.com>
2025-04-24 18:18:13 +0200

    :art: Add snackbar and animations

--- app/src/main/java/com/carlosjimz87/basfnetworkbatterymonitor/MainActivity.kt
+++ app/src/main/java/com/carlosjimz87/basfnetworkbatterymonitor/MainActivity.kt
@@ -7,9 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.carlosjimz87.basfnetworkbatterymonitor.data.models.NetworkStatus
 import com.carlosjimz87.basfnetworkbatterymonitor.ui.main.MainScreen
 import com.carlosjimz87.basfnetworkbatterymonitor.ui.main.MainViewModel
 import com.carlosjimz87.basfnetworkbatterymonitor.ui.theme.BASFNetworkBatteryMonitorTheme
@@ -23,15 +29,44 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             BASFNetworkBatteryMonitorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                    val viewModel: MainViewModel = koinViewModel()
-                    val status by viewModel.uiState.collectAsState()
+                val viewModel: MainViewModel = koinViewModel()
+                val status by viewModel.uiState.collectAsState()
+                val snackbarHostState = remember { SnackbarHostState() }
+                val previousStatus = remember { mutableStateOf(status) }
 
+                LaunchedEffect(status?.network) {
+                    val previous = previousStatus.value?.network
+                    val current = status?.network
+
+                    shouldShowSnackMessage(previous, current, snackbarHostState)
+
+                    previousStatus.value = status
+                }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    snackbarHost = { SnackbarHost(snackbarHostState) }
+                ) { innerPadding ->
                     status?.let {
-                        MainScreen(modifier = Modifier.padding(innerPadding), state = it)
+                        MainScreen(
+                            modifier = Modifier.padding(innerPadding),
+                            state = it
+                        )
                     }
                 }
+            }
+        }
+    }
+
+    private suspend fun shouldShowSnackMessage(
+        previous: NetworkStatus?,
+        current: NetworkStatus?,
+        snackbarHostState: SnackbarHostState
+    ) {
+        if (previous != null && current != null && current != previous) {
+            if (!current.connected) { // only showing lost connection for better UX
+                snackbarHostState.showSnackbar("❌ Lost network connection")
             }
         }
     }
